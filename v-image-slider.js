ad5aa698f8a0f3ee89ddcc461004403f91cababf
rogozinds <rogozinds@gmail.com>
2014-11-11 20:54:37 +0200

    Working slider to the right, need refactor

--- v-image-slider.js
+++ v-image-slider.js
@@ -1,5 +1,6 @@
 
 Polymer('v-image-slider', {
+  currentIndex: 0,
   ready: function() {
     this.addEventListener('scroll-right-action',function(e){
       scrollRight();
@@ -9,15 +10,24 @@ Polymer('v-image-slider', {
     this.fire('scroll-right-action',{msg: "scroll event"});
   },
   scrollRight: function() {
+    var width=200;
     var elem =this.shadowRoot.querySelector('li');
     var elems= this.shadowRoot.querySelectorAll('li');
+    console.log(elems[1].getAttribute("left"));
+    console.log(this.currentIndex);
+    var curElem=elems[this.currentIndex];
+    var nextIndex=this.currentIndex+1;
+    var curPos=-200*this.currentIndex;
+    var nextPos=-1*(200*nextIndex);
     var animations=[];
+    var animationProps={direction: "alternate",duration: 1000,iterations:1,fill: 'forwards'};
+    var animationTransition=[
+      {left: curPos+'px'}, { left: nextPos+'px'}
+    ];
     for(i=0;i<elems.length;i++) {
-      animations.push(new Animation(elems[i], [
-        {left: '0px'}, { left: '-200px'}
-      ],{direction: "alternate",duration: 1000,iterations:1,fill: 'forwards'}));
-
+      animations.push(new Animation(elems[i],animationTransition,animationProps));
     }
     var player=document.timeline.play(new AnimationGroup(animations));
+    this.currentIndex++;
   }
 });
