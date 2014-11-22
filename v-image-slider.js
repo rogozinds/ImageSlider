27272245041beb24fc4d04a17826fe681564f346
rogozinds <rogozinds@gmail.com>
2014-11-22 18:26:59 +0200

    Add scroll buttons. Add scroll left and right

--- v-image-slider.js
+++ v-image-slider.js
@@ -2,32 +2,49 @@
 Polymer('v-image-slider', {
   currentIndex: 0,
   ready: function() {
-    this.addEventListener('scroll-right-action',function(e){
-      scrollRight();
-    })
-  },
-  testclick: function() {
-    this.fire('scroll-right-action',{msg: "scroll event"});
+
   },
   scrollRight: function() {
     var width=200;
-    var elem =this.shadowRoot.querySelector('li');
     var elems= this.shadowRoot.querySelectorAll('li');
-    console.log(elems[1].getAttribute("left"));
-    console.log(this.currentIndex);
-    var curElem=elems[this.currentIndex];
-    var nextIndex=this.currentIndex+1;
-    var curPos=-200*this.currentIndex;
-    var nextPos=-1*(200*nextIndex);
-    var animations=[];
-    var animationProps={direction: "alternate",duration: 1000,iterations:1,fill: 'forwards'};
-    var animationTransition=[
-      {left: curPos+'px'}, { left: nextPos+'px'}
-    ];
-    for(i=0;i<elems.length;i++) {
-      animations.push(new Animation(elems[i],animationTransition,animationProps));
+    console.log(this.currentIndex +","+elems.length);
+    if (this.currentIndex < elems.length-1) {
+      var curElem=elems[this.currentIndex];
+      var nextIndex=this.currentIndex+1;
+      var curPos=-width*this.currentIndex;
+      var nextPos=-1*(width*nextIndex);
+      var animations=[];
+      var animationProps={direction: "alternate",duration: 1000,iterations:1,fill: 'forwards'};
+      var animationTransition=[
+        {left: curPos+'px'}, { left: nextPos+'px'}
+      ];
+      for(i=0;i<elems.length;i++) {
+        animations.push(new Animation(elems[i],animationTransition,animationProps));
+      }
+      var player=document.timeline.play(new AnimationGroup(animations));
+      this.currentIndex++;
     }
-    var player=document.timeline.play(new AnimationGroup(animations));
-    this.currentIndex++;
+  },
+  scrollLeftEvent: function() {
+    var width=200;
+    var elems= this.shadowRoot.querySelectorAll('li');
+    if (this.currentIndex > 0) {
+
+      var curElem=elems[this.currentIndex];
+      var nextIndex=this.currentIndex-1;
+      var curPos=-width*this.currentIndex;
+      var nextPos=-1*(width*nextIndex);
+      var animations=[];
+      var animationProps={direction: "alternate",duration: 1000,iterations:1,fill: 'forwards'};
+      var animationTransition=[
+        {left: curPos+'px'}, { left: nextPos+'px'}
+      ];
+      for(i=0;i<elems.length;i++) {
+        animations.push(new Animation(elems[i],animationTransition,animationProps));
+      }
+      console.log(curPos +","+nextPos);
+      var player=document.timeline.play(new AnimationGroup(animations));
+      this.currentIndex--;
+    }
   }
 });
