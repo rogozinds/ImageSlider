4ec647a9caefbfa08999c3ac8fa3f444cee66627
rogozinds <rogozinds@gmail.com>
2014-11-23 23:49:08 +0200

    Add dynamic images adding support

--- v-image-slider.js
+++ v-image-slider.js
@@ -1,18 +1,23 @@
 
 Polymer('v-image-slider', {
   currentIndex: 0,
+  nImages: 0,
+  sliderWidth: 200,
+  created: function() {
+          this.images = [];
+  },
   ready: function() {
-
+    this.nImages=this.shadowRoot.querySelectorAll('li').length;
+    var ulElem = this.shadowRoot.querySelectorAll('ul')[0];
+    ulElem.style.width=this.sliderWidth*this.nImages+"px";
   },
   scrollRight: function() {
-    var width=200;
     var elems= this.shadowRoot.querySelectorAll('li');
-    console.log(this.currentIndex +","+elems.length);
     if (this.currentIndex < elems.length-1) {
       var curElem=elems[this.currentIndex];
       var nextIndex=this.currentIndex+1;
-      var curPos=-width*this.currentIndex;
-      var nextPos=-1*(width*nextIndex);
+      var curPos=-this.sliderWidth*this.currentIndex;
+      var nextPos=-1*(this.sliderWidth*nextIndex);
       var animations=[];
       var animationProps={direction: "alternate",duration: 1000,iterations:1,fill: 'forwards'};
       var animationTransition=[
@@ -26,14 +31,13 @@ Polymer('v-image-slider', {
     }
   },
   scrollLeftEvent: function() {
-    var width=200;
     var elems= this.shadowRoot.querySelectorAll('li');
     if (this.currentIndex > 0) {
 
       var curElem=elems[this.currentIndex];
       var nextIndex=this.currentIndex-1;
-      var curPos=-width*this.currentIndex;
-      var nextPos=-1*(width*nextIndex);
+      var curPos=-this.sliderWidth*this.currentIndex;
+      var nextPos=-1*(this.sliderWidth*nextIndex);
       var animations=[];
       var animationProps={direction: "alternate",duration: 1000,iterations:1,fill: 'forwards'};
       var animationTransition=[
@@ -42,7 +46,6 @@ Polymer('v-image-slider', {
       for(i=0;i<elems.length;i++) {
         animations.push(new Animation(elems[i],animationTransition,animationProps));
       }
-      console.log(curPos +","+nextPos);
       var player=document.timeline.play(new AnimationGroup(animations));
       this.currentIndex--;
     }
