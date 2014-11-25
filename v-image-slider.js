d476d6a2f0dff42a2eee73db3ffef642f28e7c3f
rogozinds <rogozinds@gmail.com>
2014-11-25 19:46:39 +0200

    Add dynamic set of slider width and height

--- v-image-slider.js
+++ v-image-slider.js
@@ -2,22 +2,31 @@
 Polymer('v-image-slider', {
   currentIndex: 0,
   nImages: 0,
-  sliderWidth: 200,
   created: function() {
+      var DEFAULT_WIDTH=200;
+      var DEFAULT_HEIGHT=200;
           this.images = [];
+          //if widht is not null nor undefined
+          if(!this.width) {
+            this.width=DEFAULT_WIDTH;
+          }
+          //if height is not null nor undefined
+          if(!this.height) {
+            this.height=DEFAULT_HEIGHT;
+          }
   },
   ready: function() {
     this.nImages=this.shadowRoot.querySelectorAll('li').length;
     var ulElem = this.shadowRoot.querySelectorAll('ul')[0];
-    ulElem.style.width=this.sliderWidth*this.nImages+"px";
+    ulElem.style.width=this.width*this.nImages+"px";
   },
   scrollRight: function() {
     var elems= this.shadowRoot.querySelectorAll('li');
     if (this.currentIndex < elems.length-1) {
       var curElem=elems[this.currentIndex];
       var nextIndex=this.currentIndex+1;
-      var curPos=-this.sliderWidth*this.currentIndex;
-      var nextPos=-1*(this.sliderWidth*nextIndex);
+      var curPos=-this.width*this.currentIndex;
+      var nextPos=-1*(this.width*nextIndex);
       var animations=[];
       var animationProps={direction: "alternate",duration: 1000,iterations:1,fill: 'forwards'};
       var animationTransition=[
@@ -33,11 +42,10 @@ Polymer('v-image-slider', {
   scrollLeftEvent: function() {
     var elems= this.shadowRoot.querySelectorAll('li');
     if (this.currentIndex > 0) {
-
       var curElem=elems[this.currentIndex];
       var nextIndex=this.currentIndex-1;
-      var curPos=-this.sliderWidth*this.currentIndex;
-      var nextPos=-1*(this.sliderWidth*nextIndex);
+      var curPos=-this.width*this.currentIndex;
+      var nextPos=-1*(this.width*nextIndex);
       var animations=[];
       var animationProps={direction: "alternate",duration: 1000,iterations:1,fill: 'forwards'};
       var animationTransition=[
